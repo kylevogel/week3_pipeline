929f13d36b716dfb4c3342acceff28a86a2364a4
Mattcben1 <bxp6yd@virginia.edu>
2026-02-03 21:14:59 -0500

    Revise dataset questions and metrics
    Updated questions and insights related to College Completion and Job Placement datasets.

--- questions1_3.py
+++ questions1_3.py
@@ -1,9 +1,14 @@
-# Answer Questions Here
-Step one: Review these two datasets and brainstorm problems that could be addressed with the dataset. Identify a question for each dataset.
- 
-                                                                                                                         
-                                                                                                                         
-                                                                                                                         
-Write a generic question that this dataset could address.
-What is an independent Business Metric for your problem? 
-What do your instincts tell you about the data? Can it address your problem? What areas/items are you worried about?
+# College Completion Dataset
+
+# 1) Are higher graduation rates associated with higher levels of funding per student?
+# 2) I believe the best Independent Business Metric for this problem would be the perecent of pell grant recipients as a measurement of student socioeconomic status
+# ^ I'm curious to see if higher graduation rates are related to lower pell recipients per university.
+# 3) My instincts tell me the data is heavily numerical consisting of tons of categories. This makes me thin whether or not I can narrow down this data.
+# ^ I do see some missing data which I'm rather worried about, but overall I think this set can asnwer my question with great detail.
+
+
+# Job Placement Dataset
+
+# 1) Write a generic question that this dataset could address.
+# 2) What is an independent Business Metric for your problem? 
+# 3) What do your instincts tell you about the data? Can it address your problem? What areas/items are you worried about?
