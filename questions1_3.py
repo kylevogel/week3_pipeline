5e89db918f7b106f71d743f02134964a1f235d46
Mattcben1 <bxp6yd@virginia.edu>
2026-02-04 06:13:45 +0000

    Update answers for Questions 1–3

--- questions1_3.py
+++ questions1_3.py
@@ -6,9 +6,29 @@
 # 3) My instincts tell me the data is heavily numerical consisting of tons of categories. This makes me thin whether or not I can narrow down this data.
 # ^ I do see some missing data which I'm rather worried about, but overall I think this set can asnwer my question with great detail.
 
+# %%
+# Exploring and getting a feel for the data and its details.
+import pandas as pd
+df =  pd.read_csv("Data/cc_institution_details.csv")
+df.head()
+df.info()
+df.shape
+# Upon further observation, I see a heavy amount of non-null and strings
+# The rest will be moved to question_4.py
+
+
+
 
 # Job Placement Dataset
 
-# 1) Write a generic question that this dataset could address.
-# 2) What is an independent Business Metric for your problem? 
-# 3) What do your instincts tell you about the data? Can it address your problem? What areas/items are you worried about?
+# 1) How do academic and demographic factors whether a student will be placed or not
+# 2) A good IBM would be the placement rate of students and % of academic/demographic diversity 
+# 3) My instincts tell me that the data is fairly numerical. However, I am worried about the missing data in certain columns.
+# %%
+# Exploring the dataset
+import pandas as pd
+pd.set_option("display.max_rows", None)
+df2 = pd.read_csv("Data/job_placement.csv")
+df2.head()
+df2.info()
+# Above half are strings...hmm
